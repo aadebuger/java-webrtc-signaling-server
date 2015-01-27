24cf6ed38563a1e8fde81aa8912372ff4f31b57c
aadebuger <zhvxxh@gmail.com>
2015-01-27 22:46:20 +0800

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,1 +1,1 @@
-%JAVA_HOME%\bin\java -classpath "target\SignalingServer-1.0-SNAPSHOT.jar;target\lib\*" com.webrtcexample.signaler.Main
+java  -classpath "target/SignalingServer-1.0-SNAPSHOT.jar;target/lib/*" com.webrtcexample.signaler.Main
